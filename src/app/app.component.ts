3392744c1a043ad9028ee5e56c0e55d32b3f4167
Kumar Shan <30646609+kumaruidivloper@users.noreply.github.com>
2019-08-03 22:12:42 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Simple-NGXS-CRUD-APP-V2';
+  title = 'Simple-NGRX-CRUD-APP-V2';
 }
